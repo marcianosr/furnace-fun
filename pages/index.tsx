7e3b16f408079a85f9b0d6d14ec229a506631f08
Marciano Schildmeijer <marciano@kabisa.nl>
2022-06-12 22:23:45 +0200

    Remove GTM

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import React, { useEffect } from "react";
-import TagManager from "react-gtm-module";
 import LightCanvas from "../components/LightCanvas";
 import QuestionContainer from "../components/QuestionContainer";
 import styles from "../styles/Home.module.css";
@@ -10,19 +9,7 @@ type Props = {
 	questions: any;
 };
 
-const tagManagerArgs = {
-	gtmId: "GTM-5GJZ4MF",
-	dataLayer: {
-		userId: "001",
-		userProject: "project",
-		page: "home",
-	},
-	dataLayerName: "PageDataLayer",
-};
-
 const Home: NextPage<Props> = ({ questions }) => {
-	console.log(questions.length);
-
 	return (
 		<div className={styles.container}>
 			<Head>
